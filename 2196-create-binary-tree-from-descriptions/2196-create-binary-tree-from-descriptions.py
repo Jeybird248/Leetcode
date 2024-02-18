172d5c7c46ac2816f6569c19cffaeca32a6efe1d
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-02-18 11:55:23 -0600

    Time: 1570 ms (77.58%), Space: 26.2 MB (67.62%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        nodes = {}
+        childs = set()
+        parents = set()
+
+        for parent, child, left in descriptions:
+            childs.add(child)
+            parents.add(parent)
+            
+            if parent in nodes:
+                node = nodes[parent]
+            else:
+                node = TreeNode(parent)
+                nodes[parent] = node
+
+            if child in nodes:
+                cnode = nodes[child]
+            else:
+                cnode = TreeNode(child)
+                nodes[child] = cnode
+
+            if left:
+                node.left = cnode
+            else:
+                node.right = cnode
+
+        return nodes[list(parents - childs)[0]]
