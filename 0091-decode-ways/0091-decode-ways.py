2e01ce71d20795fc058adb8aafaad9124e329c97
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-03-02 21:44:41 -0600

    Time: 31 ms (88.03%), Space: 16.6 MB (74.12%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,30 @@
+"""
+2. Initialize a dynamic programming array dp of size n + 1, where n is the length of the input string. Set dp[0] and dp[1] to 1, as there is one way to decode an empty string and a string of length 1.
+3. Iterate through the string starting from index 2 up to n + 1.
+
+    Convert the current one-digit and two-digit substrings to integers.
+    If the one-digit substring is not '0', update dp[i] by adding dp[i - 1] because we can consider the current digit as a single character.
+    If the two-digit substring is between 10 and 26 (inclusive), update dp[i] by adding dp[i - 2] because we can consider the current two digits as a single character.
+
+4.The final result is stored in dp[n], where n is the length of the input string.
+"""
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s == "" or s[0] =="0":
+            return 0
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] =  1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            one = int(s[i - 1])
+            two = int(s[i - 2: i])
+            
+            if one != 0:
+                dp[i] += dp[i - 1]
+
+            if 10 <= two <= 26:
+                dp[i] += dp[i - 2]
+
+        return dp[n]
