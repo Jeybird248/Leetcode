e647b52c4615c7c7794b172e31f9a301d82d5c1a
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-03-21 12:34:51 -0500

    Time: 466 ms (95.13%), Space: 23.6 MB (64.24%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -7,9 +7,8 @@ class Solution:
         bought = prices[0] + fee  # Initialize bought with the first price plus fee
         
         for price in prices[1:]:  # Start iterating from the second price
-            if price + fee < bought:  # If the current price is lower than the cost of buying
-                bought = price + fee  # Update the bought price
-            elif price > bought:  # If the current price is higher than the bought price
+            bought = min(bought, price + fee)
+            if price > bought:  # If the current price is higher than the bought price
                 profit += price - bought  # Add the profit to the total profit
                 bought = price  # Update the bought price
         
