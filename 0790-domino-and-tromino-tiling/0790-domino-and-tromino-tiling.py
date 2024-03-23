a65c111eb84bf492055d95a061d899f7b7b6bbd9
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-03-23 17:34:41 -0500

    Time: 39 ms (51.80%), Space: 16.6 MB (59.07%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,19 +1,8 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        # 0 = 0
-        # 1 = 1
-        # 2 = 1 * 2 + 0 = 2
-        # 3 = 2 * 2 + 1 = 5
-        # 4 = 5 * 2 + 1 = 11
-        # 5 = 11 * 2 + 2 =  24
-        # 6 = 24 * 2 + 5 = 53
-        # 7 = 53 * 2 + 11 = 117
-        dp = [0] * (n + 5)
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 5
-        dp[4] = 11
-        for i in range(5, n + 1):
-            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % (10**9 + 7)
-        return dp[n]
+        if n <= 2:
+            return n
+        a, b, c = 1, 1, 2
+        for _ in range(3, n + 1):
+            a, b, c = b, c, (c * 2 + a) % (10**9 + 7)
+        return c
