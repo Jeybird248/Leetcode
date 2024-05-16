9ccb7c73ca5ff3ebb8abf84b4bc2140df841b25e
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-05-16 11:43:05 -0500

    Time: 35 ms (61.77%), Space: 16.8 MB (11.83%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,10 +1,19 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if s == t:
+        if len(s) > len(t):
+            return False
+        if not s:
             return True
-        s_point = t_point = 0
-        while t_point < len(t) and s_point < len(s):
-            if t[t_point] == s[s_point]:
-                s_point += 1
-            t_point += 1
-        return s_point >= len(s)
+        start = end = 0
+        for i in range(len(t)):
+            # print("comparing start to i", s[start], t[i])
+            # print()
+            if s[start] == t[i]:
+                end = start
+                for j in range(i, len(t)):
+                    # print("comparing end to j", s[end], t[j])
+                    if end < len(s) and t[j] == s[end]:
+                        end += 1
+                    if end == len(s):
+                        return True
+        return False
