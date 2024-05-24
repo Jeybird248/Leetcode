ce2b217f58c8a02e82ba8d2b2bb01e1f43ebbbab
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-05-24 16:15:41 -0500

    Time: 30 ms (97.17%), Space: 17.6 MB (84.52%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -8,4 +8,20 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        dq = deque([root]) # deque is optimized for popping 
+        num_node_level = 1 # current number of nodes left in level
+        levels = 0
+        while dq:
+            node = dq.popleft()
+            if node.left: 
+                dq.append(node.left)
+            if node.right:
+                dq.append(node.right)
+            num_node_level -= 1
+            if num_node_level == 0: # if all nodes in the level has been sold
+                levels += 1 # go to next level
+                num_node_level = len(dq) # count number of nodes in next level
+                
+        return levels
