bec8dc7b94ed8fae89b3f525e86055c0b7292a99
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-05-24 16:47:20 -0500

    Time: 42 ms (16.02%), Space: 16.6 MB (43.93%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -6,16 +6,17 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.stack1 = []
-        self.stack2 = []
+        self.arr1 = []
+        self.arr2 = []
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def helper(root, stack):
+        def traversal(root, arr):
             if not root:
-                return None
-            helper(root.left, stack)
+                return
             if not root.left and not root.right:
-                stack.append(root.val)
-            helper(root.right, stack)
-        helper(root1, self.stack1)
-        helper(root2, self.stack2)
-        return self.stack1 == self.stack2
+                arr.append(root.val)
+            traversal(root.left, arr)
+            traversal(root.right, arr)
+            
+        traversal(root1, self.arr1)
+        traversal(root2, self.arr2)
+        return self.arr1 == self.arr2
