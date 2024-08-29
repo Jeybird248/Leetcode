f75e2234f3208f700347dab5c328e5a0feaeb930
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-08-28 17:32:44 -0700

    Time: 466 ms (46.43%), Space: 35.2 MB (38.28%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,9 +1,7 @@
-from typing import List
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         output = []
-
+        
         def backtrack(start, path):
             # base case: if start index reaches the end of the string, add the path to output
             if start == len(s):
@@ -13,13 +11,8 @@ class Solution:
             # explore all possible end positions for substring
             for end in range(start + 1, len(s) + 1):
                 # if the substring is a palindrome, recursively continue partitioning
-                print(s[start:end], path, start, end)
-                if is_palindrome(s[start:end]):
+                if (s[start:end] == s[start:end][::-1]):
                     backtrack(end, path + [s[start:end]])
-
-        def is_palindrome(substring: str) -> bool:
-            return substring == substring[::-1]
-
-        # start backtracking from the beginning of the string
+        
         backtrack(0, [])
         return output
