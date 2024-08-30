65c81e2ba3052d1d82c66fdeafca04b3d0ac56d2
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-08-30 13:15:13 -0700

    Time: 9306 ms (5.78%), Space: 16.3 MB (99.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,30 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        """
-        Think about how you can recursively build subsets by deciding for each element 
-        in the array whether to include it in the current subset or not. Start from an 
-        empty set and progressively add or exclude each element. This approach is called 
-        "backtracking." How can you ensure you explore all possibilities without repeating subsets?
-        """
+        output = []
         
-        result = []  # Initialize the result list to store all subsets
-
-        def backtrack(index, current_subset):
-            # Base case: if we have considered all elements
-            if index == len(nums):
-                result.append(current_subset[:])  # Add a copy of the current subset to the result
+        def recursion(curr_arr):
+            if curr_arr not in output:
+                output.append(curr_arr)
+            if not curr_arr:
                 return
-
-            # Recursive case: exclude the current element
-            backtrack(index + 1, current_subset)
-
-            # Recursive case: include the current element
-            current_subset.append(nums[index])
-            backtrack(index + 1, current_subset)
-
-            # Backtrack: remove the last element added for the next call
-            current_subset.pop()
-
-        # Start the backtracking from index 0 and with an empty subset
-        backtrack(0, [])
-        return result
+            for idx in range(len(curr_arr)):
+                recursion(curr_arr[:idx] + curr_arr[idx + 1:])
+        recursion(nums)
+        return output
