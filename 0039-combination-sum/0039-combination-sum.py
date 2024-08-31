92e36901ec3f28a5a5f447f129159b2056fbb24f
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-08-31 06:13:52 -0700

    Time: 110 ms (9.40%), Space: 16.7 MB (11.15%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,25 +1,18 @@
-from typing import List
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        output = []
-        
-        def backtrack(remaining, curr_combination, index):
-            # base case: if the remaining target is 0, we found a valid combination
-            if remaining == 0:
-                output.append(curr_combination)
+        self.output = []
+        def backtrack(currIdx, path, curr_target):
+            print(path, curr_target)
+            if curr_target == 0:
+                self.output.append(path.copy())
+            if curr_target < 0:
                 return
             
-            # if remaining target is less than 0, no valid combination is possible
-            if remaining < 0:
-                return
+            for i in range(currIdx, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, curr_target - candidates[i])
+                path.pop()
             
-            # explore further elements from the current index onwards
-            for i in range(index, len(candidates)):
-                # recurse with updated parameters: new target, updated combination, and current index
-                backtrack(remaining - candidates[i], curr_combination + [candidates[i]], i)
+        backtrack(0, [], target)
         
-        # initial call to backtrack with full target, empty combination, and starting index 0
-        backtrack(target, [], 0)
-        
-        return output
+        return self.output
