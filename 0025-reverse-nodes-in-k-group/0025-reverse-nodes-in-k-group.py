071f10dad78a422ae574148333f4722ab39f0835
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-04 06:29:55 -0700

    Time: 38 ms (84.17%), Space: 17.4 MB (92.00%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,10 +5,10 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # problem breaks down to reversing linkedlist
+        
         def reverse(start, end):
-            prev = None
             curr = start
+            prev = None
             while curr != end:
                 temp = curr.next
                 curr.next = prev
@@ -17,33 +17,24 @@ class Solution:
             return prev
         
         dummy = ListNode(0)
-        
         dummy.next = head
         curr = dummy
         
         length = 0
-        temp = head
-        while temp:
-            temp = temp.next
+        curr2 = head
+        while curr2:
+            curr2 = curr2.next
             length += 1
             
-        while length >= k:
-            start = curr.next
-            end = start
+        for i in range(length // k):
+            start = end = curr.next
             for i in range(k):
                 end = end.next
-            
-            # Reverse the k-group
-            reversed_head = reverse(start, end)
-            
-            # Connect reversed part to the previous section
-            curr.next = reversed_head
+            prev_head = reverse(start, end)
+
+            curr.next = prev_head
             start.next = end
             
-            # Move the pointers to the next section
             curr = start
-            
-            # Reduce the length by k
-            length -= k
             
         return dummy.next
