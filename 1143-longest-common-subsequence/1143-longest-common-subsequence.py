a198b9ff9df41ee74aaf8242938832d7ece56dc9
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-05 11:57:30 -0500

    Time: 519 ms (46.44%), Space: 42.5 MB (44.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m, n = len(text1), len(text2)
+        dp = [[0 for _ in range(len(text1) + 1)] for _ in range(len(text2) + 1)]
+
         
-        dp = [[0] * (n + 1) for _ in range(m + 1)] # length looking at neither string is 0
-        
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if text1[i - 1] == text2[j - 1]: # looking at indexes i and j
-                    dp[i][j] = dp[i - 1][j - 1] + 1 # max characters before are matching + this current character
-                else:  
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) # take max characters before matching 
-        
-        return dp[m][n]
+        for i in range(1, len(text2) + 1):
+            for j in range(1, len(text1) + 1):
+                if text2[i - 1] == text1[j - 1]:  # Compare the correct indices
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        # The result is in the bottom-right corner of the dp table
+        return dp[len(text2)][len(text1)]
