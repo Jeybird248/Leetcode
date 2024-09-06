1a196998495399fa8d8ae37c897e27881f2b8dd0
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-06 10:27:42 -0500

    Time: 27 ms (98.36%), Space: 16.5 MB (47.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,5 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix[:] = map(list, zip(*matrix))
-        matrix[:] = [row[::-1] for row in matrix]
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row.reverse()
