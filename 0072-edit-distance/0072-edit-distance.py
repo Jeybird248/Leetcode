ab0a92d9e7bfe4b0cf4973aec3b6cb67faaefbe3
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-06 10:52:03 -0500

    Time: 116 ms (21.79%), Space: 20.2 MB (47.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,21 +1,17 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
+        dp = [[0 for _ in range(len(word1) + 1)] for _ in range(len(word2) + 1)]
         
-        # Create a 2D DP array with (m+1) x (n+1) dimensions
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-        for i in range(1, m + 1):
-            dp[i][0] = i
-        
-        for i in range(1, n + 1):
+        for i in range(len(word1) + 1):
             dp[0][i] = i
+        for i in range(len(word2) + 1):
+            dp[i][0] = i
             
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
                 if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
+                    dp[j][i] = dp[j - 1][i - 1]
                 else:
-                    dp[i][j] = min(dp[i-1][j] + 1, dp[i][j - 1] + 1, dp[i-1][j-1] + 1)
-        
-        return dp[m][n]
+                    dp[j][i] = min(dp[j - 1][i], dp[j][i - 1], dp[j - 1][i - 1]) + 1
+
+        return dp[len(word2)][len(word1)]
