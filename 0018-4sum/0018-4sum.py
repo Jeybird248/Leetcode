7a5719149e4d89adaf9ecd5ba44f981ff86da5a4
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-18 08:42:12 -0500

    Time: 680 ms (18.22%), Space: 16.5 MB (70.19%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -3,7 +3,11 @@ class Solution:
         nums.sort()
         output = []
         for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
                 left, right = j + 1, len(nums) - 1
                 
                 while left < right:
