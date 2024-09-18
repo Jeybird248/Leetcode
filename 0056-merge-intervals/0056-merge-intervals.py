0dfdeddd6faab4dd5915bde42eb7c97010ad3cc8
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-18 08:01:39 -0500

    Time: 129 ms (28.04%), Space: 21.5 MB (8.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,7 +1,7 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        length = len(intervals)
         intervals.sort(key=lambda x: x[0])
-        output = []
         left, right = intervals[0][0], intervals[0][1]
         
         for idx, interval in enumerate(intervals[1:]):
@@ -9,10 +9,10 @@ class Solution:
                 right = max(right, interval[1])
                 left = min(left, interval[0])
             else:
-                output.append([left, right])
+                intervals.append([left, right])
                 left = interval[0]
                 right = interval[1]
                 
-        output.append([left, right])
-        return output
+        intervals.append([left, right])
+        return intervals[length:]
         
