982d169de32b5c9ab18dfb196d16641ded75472c
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-19 10:35:35 -0500

    Time: 2370 ms (5.00%), Space: 38.5 MB (52.06%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -3,32 +3,18 @@ class MedianFinder:
     def __init__(self):
         self.arr = []
         
+
     def addNum(self, num: int) -> None:
-        left, right = 0, len(self.arr) - 1
-        
-        # Binary search for the correct insertion point
-        while left <= right:
-            mid = (left + right) // 2
-            if self.arr[mid] < num:
-                left = mid + 1
-            else:
-                right = mid - 1
-        
-        # Insert the number at the correct position
-        self.arr.insert(left, num)
-        
+        self.arr.append(num)
+
     def findMedian(self) -> float:
-        n = len(self.arr)
-        if n % 2 == 1:
-            return self.arr[n // 2]
-        else:
-            return (self.arr[n // 2 - 1] + self.arr[n // 2]) / 2
+        self.arr.sort()
+        if len(self.arr) % 2 != 0:
+            return self.arr[len(self.arr) // 2]
+        return (self.arr[len(self.arr) // 2] + self.arr[len(self.arr) // 2 - 1]) / 2
 
 
-# Example of usage:
+# Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
-# obj.addNum(1)
-# obj.addNum(2)
-# print(obj.findMedian()) # Outputs 1.5
-# obj.addNum(3)
-# print(obj.findMedian()) # Outputs 
+# obj.addNum(num)
+# param_2 = obj.findMedian()
