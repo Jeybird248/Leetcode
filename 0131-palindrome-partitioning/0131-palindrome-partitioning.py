dbb079c561b99659a1db531b133bbf5c95b3f48a
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-21 09:06:50 -0500

    Time: 475 ms (28.74%), Space: 35.4 MB (10.80%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,13 +1,16 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         output = []
-        def backtrack(currIdx, path):
-            if currIdx == len(s):
-                output.append(path)
-                return
-            for i in range(currIdx + 1, len(s) + 1):
-                if s[currIdx:i] == s[currIdx:i][::-1]:
-                    backtrack(i, path + [s[currIdx:i][::-1]])
-                
-        backtrack(0, [])
+        
+        def backtrack(curr_arr, index):
+            if index == len(s):
+                output.append(curr_arr[:])
+            else:
+                for i in range(index + 1, len(s) + 1):
+                    if s[index: i] == s[index: i][::-1]:
+                        curr_arr.append(s[index: i])
+                        backtrack(curr_arr, i)
+                        curr_arr.pop()
+                        
+        backtrack([], 0)
         return output
