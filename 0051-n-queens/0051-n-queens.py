7f5b3eb7245508e5dc447b9d48c33da8fbd547c2
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-23 08:18:24 -0500

    Time: 67 ms (37.45%), Space: 17.2 MB (6.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,7 +1,5 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        output = []
-        
         def is_valid(board, row, col):
             for i in range(row):
                 if board[i][col] == 'Q':
@@ -23,15 +21,17 @@ class Solution:
             
             return True
         
+        output = []
+        board = [["." for _ in range(n)] for _ in range(n)]
+        
         def backtrack(row):
-            if row == len(board):
+            if row == n:
                 output.append(["".join(r) for r in board])
-                return
-            for col in range(len(board[0])):
-                if is_valid(board, row, col):
-                    board[row][col] = "Q"
-                    backtrack(row + 1)
-                    board[row][col] = "."
-        board = [['.' for _ in range(n)] for _ in range(n)]
+            else:
+                for col in range(n):
+                    if is_valid(board, row, col):
+                        board[row][col] = "Q"
+                        backtrack(row + 1)
+                        board[row][col] = "."
         backtrack(0)
         return output
