308af64bcb36204add8ccf8cddd3ea4c3960da37
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-09-24 16:42:12 -0500

    Time: 260 ms (39.50%), Space: 18.9 MB (61.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
-        def bfs(x, y, grid):
-            if 0 > x or x >= len(grid[0]) or y < 0 or y >= len(grid) or grid[y][x] == "0":
-                return
-            
-            grid[y][x] = "0"
-            
-            bfs(x - 1, y, grid)
-            bfs(x + 1, y, grid)
-            bfs(x, y + 1, grid)
-            bfs(x, y - 1, grid)
-
+        directions = [(1, 0), (0, 1), (0, -1), (-1, 0) ]
+        def bfs(x, y):
+            if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
+                grid[y][x] = "-1"
+                for d in directions:
+                    if 0 <= x + d[0] < len(grid[0]) and 0 <= y + d[1] < len(grid) and grid[y + d[1]][x + d[0]] == "1":
+                        bfs(x + d[0], y + d[1])
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    bfs(j, i, grid)
+                    bfs(j, i)
                     count += 1
-                    
+        
         return count
