4b851e75261658a9811b66e46e7b430033d644e6
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-10-08 11:42:20 -0500

    Time: 1069 ms (8.54%), Space: 77.7 MB (91.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,33 +1,27 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.d = {}
-        self.history = []
         self.cap = capacity
-
+        self.map = {}
+        
     def get(self, key: int) -> int:
-        if key in self.d:
-            self.history.remove(key)
-            self.history.append(key)
-        return self.d.get(key, -1)
+        if key not in self.map:
+            return -1
+        # Move the accessed key to the end to mark it as recently used
+        value = self.map.pop(key)
+        self.map[key] = value
+        return value
 
     def put(self, key: int, value: int) -> None:
-        
-        if key in self.d:
-            self.history.remove(key)
-            self.history.append(key)
-            self.d[key] = value
-        else:
-            if len(self.d) >= self.cap:
-                while True:
-                    temp =  self.history.pop(0)
-                    if temp in self.d:
-                        self.d.pop(temp)
-                        break
-            self.d[key] = value
-            self.history.append(key)
+        if key in self.map:
+            # Remove the existing key to update its position later
+            self.map.pop(key)
+        elif len(self.map) >= self.cap:
+            # Remove the least recently used key (the first key)
+            del self.map[list(self.map.keys())[0]]
+        # Insert the new key-value pair, placing it at the end
+        self.map[key] = value
 
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
