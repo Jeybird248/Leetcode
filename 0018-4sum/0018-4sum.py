ffa1e7d6a7d5f35ce0e02eed2df00ec2190d2473
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-10-09 09:42:04 -0500

    Time: 1150 ms (5.03%), Space: 16.6 MB (75.95%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,19 +2,16 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         output = []
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        
+        for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
                 left, right = j + 1, len(nums) - 1
-                
                 while left < right:
-                    if target == nums[i] + nums[j] + nums[left] + nums[right] and [nums[i], nums[j], nums[left] ,nums[right]] not in output:
-                        output.append([nums[i], nums[j], nums[left] ,nums[right]])
-                    elif target > nums[i] + nums[j] + nums[left] + nums[right]:
-                        left += 1
-                    else:
+                    if nums[i] + nums[j] + nums[left] + nums[right] == target and sorted([nums[i] , nums[j] , nums[left] , nums[right]]) not in output:
+                        output.append(sorted([nums[i] , nums[j] , nums[left] , nums[right]]))
+                        
+                    elif nums[i] + nums[j] + nums[left] + nums[right] > target:
                         right -= 1
+                    else:
+                        left += 1
         return output
